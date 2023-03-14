3f1cc381781cd25e7983390def4e74d0e1e04d8a
rbabaci1 <4rabah@gmail.com>
2023-03-14 11:54:41 -0700

    refactor the Header comp

--- portfolio/src/container/Header/Header.jsx
+++ portfolio/src/container/Header/Header.jsx
@@ -15,6 +15,7 @@ const scaleVariants = {
     },
   },
 };
+const { profile, circle, flutter, redux, sass } = images;
 
 const Header = () => (
   <div className='app__header app__flex'>
@@ -44,12 +45,12 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className='app__header-img'
     >
-      <img src={images.profile} alt='profile_bg' />
+      <img src={profile} alt='profile_bg' />
 
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
-        src={images.circle}
+        src={circle}
         alt='profile_circle'
         className='overlay_circle'
       />
@@ -60,7 +61,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className='app__header-circles'
     >
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+      {[flutter, redux, sass].map((circle, index) => (
         <div className='circle-cmp app__flex' key={`circle-${index}`}>
           <img src={circle} alt='profile_bg' />
         </div>
