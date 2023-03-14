1fea2bc9de0a449cc5cf78bb70062d01dcb7405f
rbabaci1 <4rabah@gmail.com>
2023-03-14 11:17:45 -0700

    implement the Header comp

--- portfolio/src/Wrapper/AppWrap.jsx
+++ portfolio/src/Wrapper/AppWrap.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { NavigationDots, SocialMedia } from '../components';
+// import { NavigationDots, SocialMedia } from '../components';
 
 const AppWrap = (Component, idName, classNames) =>
   function HOC() {
     return (
       <div id={idName} className={`app__container ${classNames}`}>
-        <SocialMedia />
+        {/* <SocialMedia /> */}
+
         <div className='app__wrapper app__flex'>
           <Component />
 
@@ -14,7 +15,8 @@ const AppWrap = (Component, idName, classNames) =>
             <p className='p-text'>All rights reserved</p>
           </div>
         </div>
-        <NavigationDots active={idName} />
+
+        {/* <NavigationDots active={idName} /> */}
       </div>
     );
   };
