e403188de0872c01b4a7954ec614d431decae4ba
rbabaci1 <4rabah@gmail.com>
2023-03-14 08:58:19 -0700

    create all the needed sanity schemas

--- sanity_backend/schemas/index.js
+++ sanity_backend/schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experience from './experience'
+import skills from './skills'
+import testimonials from './testimonials'
+import work from './work'
+import workExperience from './workExperience'
+
+export const schemaTypes = [
+  abouts,
+  brands,
+  contact,
+  experience,
+  skills,
+  testimonials,
+  work,
+  workExperience,
+]
