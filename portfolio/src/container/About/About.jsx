783a62786eab3c9d813d020f878e614ae492e40d
rbabaci1 <4rabah@gmail.com>
2023-03-14 17:08:39 -0700

    upload the abouts data to sanity and fetch it from the about container

--- portfolio/src/container/About/About.jsx
+++ portfolio/src/container/About/About.jsx
@@ -2,50 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
-import { images } from '../../constants';
 import './About.scss';
-// import { urlFor, client } from '../../client';
+import { urlFor, client } from '../../client';
 
 const About = () => {
-  // const [abouts, setAbouts] = useState([]);
+  const [abouts, setAbouts] = useState([]);
 
-  const { about01, about02, about03, about04 } = images;
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
 
-  // useEffect(() => {
-  //   const query = '*[_type == "abouts"]';
-
-  //   client.fetch(query).then(data => {
-  //     setAbouts(data);
-  //   });
-  // }, []);
-
-  // create me some dummy data of an array of objects to test the map function
-  const abouts = [
-    {
-      title: 'Our Mission',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan sagittis ex, in porta quam commodo in. Sed euismod massa nec urna feugiat efficitur.',
-      imgUrl: about01,
-    },
-    {
-      title: 'Meet the Team',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan sagittis ex, in porta quam commodo in. Sed euismod massa nec urna feugiat efficitur.',
-      imgUrl: about02,
-    },
-    {
-      title: 'Our Values',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan sagittis ex, in porta quam commodo in. Sed euismod massa nec urna feugiat efficitur.',
-      imgUrl: about03,
-    },
-    {
-      title: 'Our History',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan sagittis ex, in porta quam commodo in. Sed euismod massa nec urna feugiat efficitur.',
-      imgUrl: about04,
-    },
-  ];
+    client.fetch(query).then(data => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
@@ -63,8 +32,7 @@ const About = () => {
             className='app__profile-item'
             key={`${about.title}-${index}`}
           >
-            {/* <img src={urlFor(about.imgUrl)} alt={about.title} /> */}
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
 
             <h2 className='bold-text' style={{ marginTop: 15 }}>
               {about.title}
