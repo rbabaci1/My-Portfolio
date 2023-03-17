5389ebcc05a2979be8c7d37ea2b035c838753b2d
rbabaci1 <4rabah@gmail.com>
2023-03-16 18:58:54 -0700

    fix header heading spacing

--- portfolio/src/container/Header/Header.jsx
+++ portfolio/src/container/Header/Header.jsx
@@ -32,6 +32,7 @@ const Header = () => (
         <div className='app__header-badge'>
           <div className='badge-cmp app__flex'>
             <span>👋</span>
+
             <div style={{ marginLeft: 20 }}>
               <p className='p-text'>Hello, I am</p>
               <h1 className='head-text'>Rabah</h1>
