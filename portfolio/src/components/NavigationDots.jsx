3536051c6465ee497d72fd3a52a507a1bc2c3315
rbabaci1 <4rabah@gmail.com>
2023-03-16 18:41:38 -0700

    fix the position of social links and nav dots

--- portfolio/src/components/NavigationDots.jsx
+++ portfolio/src/components/NavigationDots.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NavigationDots = ({ activeSection }) => (
-  <div className='app__navigation'>
-    {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map(
-      (item, index) => (
-        <a
-          href={`#${item}`}
-          key={item + index}
-          className='app__navigation-dot'
-          style={activeSection === item ? { backgroundColor: '#313BAC' } : {}}
-        />
-      )
-    )}
-  </div>
-);
+const NavigationDots = () => {
+  const [active, setActive] = useState('home');
+
+  return (
+    <div className='app__navigation'>
+      {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map(
+        (item, index) => (
+          <a
+            onClick={() => setActive(item)}
+            href={`#${item}`}
+            key={item + index}
+            className='app__navigation-dot'
+            style={active === item ? { backgroundColor: '#313BAC' } : {}}
+          />
+        )
+      )}
+    </div>
+  );
+};
 
 export default NavigationDots;
