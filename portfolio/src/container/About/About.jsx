f0a58788651c6976efa347b5f75bfdf081f242ba
rbabaci1 <4rabah@gmail.com>
2023-03-18 18:48:03 -0700

    make skills cards tiltable

--- portfolio/src/container/About/About.jsx
+++ portfolio/src/container/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import Tilt from 'react-parallax-tilt';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
@@ -18,30 +19,48 @@ const About = () => {
 
   return (
     <>
-      <h2 className='head-text app__about-header'>
-        I Know that <span>Good Design</span> <br />
-        means <span>Good Business</span>
-      </h2>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__about-header'
+      >
+        <p className='p-text'>INTRODUCTION</p>
+        <h2 className='head-text'>Overview</h2>
+        <p className='p-text'>
+          I'm a skilled software developer with experience in TypeScript and
+          JavaScript, and expertise in frameworks like React, Node.js, and
+          Three.js. I'm a quick learner and collaborate closely with clients to
+          create efficient, scalable, and user-friendly solutions that solve
+          real-world problems. Let's work together to bring your ideas to life!
+        </p>
+      </motion.div>
+      {/* <h2 className='head-text app__about-header'>
+        Hi again! <span>Nice to meet you.</span>
+      </h2> */}
 
       <div className='app__profiles'>
         {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'spring' }}
-            className='app__profile-item'
-            key={`${about.title}-${index}`}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
+          <Tilt>
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: 'spring' }}
+              className='app__profile-item'
+              key={`${about.title}-${index}`}
+            >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
 
-            <h2 className='bold-text' style={{ marginTop: 15 }}>
-              {about.title}
-            </h2>
+              <div className='details'>
+                <h2 className='bold-text' style={{ marginTop: 15 }}>
+                  {about.title}
+                </h2>
 
-            <p className='p-text' style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
+                <p className='p-text' style={{ marginTop: 10 }}>
+                  {about.description}
+                </p>
+              </div>
+            </motion.div>
+          </Tilt>
         ))}
       </div>
     </>
