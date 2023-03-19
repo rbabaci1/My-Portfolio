cc4a4b5a9a4006677980c06dfcc43dc71c98d63f
rbabaci1 <4rabah@gmail.com>
2023-03-18 17:50:27 -0700

    .

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,13 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.logo} alt='logo' />
+        <img
+          src={images.logo}
+          alt='logo'
+          onClick={() => {
+            window.scroll(0, 0);
+          }}
+        />
       </div>
 
       <ul className='app__navbar-links'>
