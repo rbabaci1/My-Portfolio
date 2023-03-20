e83afb8eb60188c5d54aeb5b41551da498631be3
rbabaci1 <4rabah@gmail.com>
2023-03-19 19:11:11 -0700

    setup space for the 3d-earth component

--- portfolio/src/container/Footer/Footer.jsx
+++ portfolio/src/container/Footer/Footer.jsx
@@ -46,78 +46,84 @@ const Footer = () => {
   };
 
   return (
-    <>
+    <div>
       <h2 className='head-text'>
         Let's chat over a <span>coffee</span>
       </h2>
 
-      <div className='app__footer-cards'>
-        <div className='app__footer-card '>
-          <img src={images.email} alt='email' />
-
-          <a href='4rabah@gmail.com' className='p-text'>
-            4rabah@gmail.com
-          </a>
-        </div>
-
-        <div className='app__footer-card'>
-          <img src={images.mobile} alt='phone' />
-
-          <a href='tel:+1 (510) 646-7743' className='p-text'>
-            +1 (510) 646-7743
-          </a>
-        </div>
-      </div>
-
-      {!isFormSubmitted ? (
-        <div className='app__footer-form app__flex'>
-          <div className='app__flex'>
-            <input
-              className='p-text'
-              type='text'
-              placeholder='Your Name'
-              name='username'
-              value={username}
-              onChange={handleChangeInput}
-            />
-          </div>
-
-          <div className='app__flex'>
-            <input
-              className='p-text'
-              type='email'
-              placeholder='Your Email'
-              name='email'
-              value={email}
-              onChange={handleChangeInput}
-            />
-          </div>
-
-          <div>
-            <textarea
-              className='p-text'
-              placeholder='Your Message'
-              value={message}
-              name='message'
-              onChange={handleChangeInput}
-            />
-          </div>
-
-          <button type='button' className='p-text' onClick={handleSubmit}>
-            {!loading ? 'Send Message' : 'Sending...'}
-          </button>
-        </div>
-      ) : (
+      <div className='app_contact-main'>
         <div>
-          <h3 className='head-text'>Thanks for getting in touch!</h3>
+          <div className='app__footer-cards'>
+            <div className='app__footer-card '>
+              <img src={images.email} alt='email' />
+
+              <a href='4rabah@gmail.com' className='p-text'>
+                4rabah@gmail.com
+              </a>
+            </div>
+
+            <div className='app__footer-card'>
+              <img src={images.mobile} alt='phone' />
+
+              <a href='tel:+1 (510) 646-7743' className='p-text'>
+                +1 (510) 646-7743
+              </a>
+            </div>
+          </div>
+
+          {!isFormSubmitted ? (
+            <div className='app__footer-form app__flex'>
+              <div className='app__flex'>
+                <input
+                  className='p-text'
+                  type='text'
+                  placeholder='Your Name'
+                  name='username'
+                  value={username}
+                  onChange={handleChangeInput}
+                />
+              </div>
+
+              <div className='app__flex'>
+                <input
+                  className='p-text'
+                  type='email'
+                  placeholder='Your Email'
+                  name='email'
+                  value={email}
+                  onChange={handleChangeInput}
+                />
+              </div>
+
+              <div>
+                <textarea
+                  className='p-text'
+                  placeholder='Your Message'
+                  value={message}
+                  name='message'
+                  onChange={handleChangeInput}
+                />
+              </div>
+
+              <button type='button' className='p-text' onClick={handleSubmit}>
+                {!loading ? 'Send Message' : 'Sending...'}
+              </button>
+            </div>
+          ) : (
+            <div>
+              <h3 className='head-text'>Thanks for getting in touch!</h3>
+            </div>
+          )}
         </div>
-      )}
+
+        <div className='app__3d-earth'></div>
+      </div>
 
       <div className='copyright'>
         <p className='p-text'>@2023 Rabah</p>
         <p className='p-text'>All rights reserved</p>
       </div>
-    </>
+    </div>
   );
 };
 
