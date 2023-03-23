2afc453cbea93419493b85406ef75bf51a8917bb
rbabaci1 <4rabah@gmail.com>
2023-03-23 15:51:57 -0700

    delete the skills schema

--- sanity_backend/schemas/index.js
+++ sanity_backend/schemas/index.js
@@ -1,7 +1,6 @@
 import abouts from './abouts'
 import experiences from './experiences'
-import skills from './skills'
 import testimonials from './testimonials'
 import projects from './projects'
 
-export const schemaTypes = [abouts, experiences, skills, testimonials, projects]
+export const schemaTypes = [abouts, experiences, testimonials, projects]
